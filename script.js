d14c4b73b9412655ad632545d3c52774bc94c473
Mohamed Anwar <mhmdwaelanwr@gmail.com>
2026-09-16 20:38:59 +0300

    Use basic vanilla JavaScript for calculator

--- script.js
+++ script.js
@@ -1,183 +1,84 @@
-var e = React.createElement;
+var firstNumber = 0;
+var mathOperator = "";
+var startNewNumber = false;
 
-function App() {
-  var displayState = React.useState("0");
-  var display = displayState[0];
-  var setDisplay = displayState[1];
+function addNumber(number) {
+  var screen = document.getElementById("screen");
 
-  var firstState = React.useState(null);
-  var firstNumber = firstState[0];
-  var setFirstNumber = firstState[1];
-
-  var operatorState = React.useState(null);
-  var operator = operatorState[0];
-  var setOperator = operatorState[1];
-
-  var newNumberState = React.useState(false);
-  var newNumber = newNumberState[0];
-  var setNewNumber = newNumberState[1];
-
-  function addNumber(number) {
-    if (display === "Error" || newNumber === true) {
-      setDisplay(number);
-      setNewNumber(false);
-    } else if (display === "0") {
-      setDisplay(number);
-    } else if (display.length < 10) {
-      setDisplay(display + number);
-    }
+  if (screen.value == "0" || startNewNumber == true) {
+    screen.value = number;
+    startNewNumber = false;
+  } else {
+    screen.value = screen.value + number;
   }
-
-  function addDot() {
-    if (newNumber === true) {
-      setDisplay("0.");
-      setNewNumber(false);
-    } else if (display.indexOf(".") === -1) {
-      setDisplay(display + ".");
-    }
-  }
-
-  function clearAll() {
-    setDisplay("0");
-    setFirstNumber(null);
-    setOperator(null);
-    setNewNumber(false);
-  }
-
-  function chooseOperator(symbol) {
-    if (display === "Error") {
-      return;
-    }
-
-    setFirstNumber(Number(display));
-    setOperator(symbol);
-    setNewNumber(true);
-  }
-
-  function calculate() {
-    if (firstNumber === null || operator === null || display === "Error") {
-      return;
-    }
-
-    var secondNumber = Number(display);
-    var answer = 0;
-
-    if (operator === "+") {
-      answer = firstNumber + secondNumber;
-    }
-
-    if (operator === "-") {
-      answer = firstNumber - secondNumber;
-    }
-
-    if (operator === "×") {
-      answer = firstNumber * secondNumber;
-    }
-
-    if (operator === "÷") {
-      if (secondNumber === 0) {
-        setDisplay("Error");
-        setFirstNumber(null);
-        setOperator(null);
-        return;
-      }
-      answer = firstNumber / secondNumber;
-    }
-
-    answer = Math.round(answer * 100000) / 100000;
-    setDisplay(String(answer));
-    setFirstNumber(null);
-    setOperator(null);
-    setNewNumber(true);
-  }
-
-  function changeSign() {
-    if (display === "0" || display === "Error") {
-      return;
-    }
-
-    if (display.charAt(0) === "-") {
-      setDisplay(display.substring(1));
-    } else {
-      setDisplay("-" + display);
-    }
-  }
-
-  function percentage() {
-    if (display !== "Error") {
-      setDisplay(String(Number(display) / 100));
-    }
-  }
-
-  function deleteLast() {
-    if (display === "Error" || newNumber === true) {
-      return;
-    }
-
-    if (display.length <= 1) {
-      setDisplay("0");
-    } else {
-      setDisplay(display.substring(0, display.length - 1));
-    }
-  }
-
-  function makeButton(text, cssClass, clickFunction) {
-    return e(
-      "button",
-      {
-        className: cssClass,
-        onClick: clickFunction
-      },
-      text
-    );
-  }
-
-  var smallText = "";
-  if (firstNumber !== null && operator !== null) {
-    smallText = firstNumber + " " + operator;
-  }
-
-  return e(
-    "div",
-    { className: "calculator" },
-    e("h1", { className: "title" }, "Calculator"),
-    e(
-      "div",
-      { className: "screen" },
-      e("div", { className: "operation" }, smallText),
-      e("div", { className: "result" }, display)
-    ),
-    e(
-      "div",
-      { className: "buttons" },
-      makeButton("AC", "gray", clearAll),
-      makeButton("+/-", "gray", changeSign),
-      makeButton("%", "gray", percentage),
-      makeButton("÷", "operator", function () { chooseOperator("÷"); }),
-
-      makeButton("7", "", function () { addNumber("7"); }),
-      makeButton("8", "", function () { addNumber("8"); }),
-      makeButton("9", "", function () { addNumber("9"); }),
-      makeButton("×", "operator", function () { chooseOperator("×"); }),
-
-      makeButton("4", "", function () { addNumber("4"); }),
-      makeButton("5", "", function () { addNumber("5"); }),
-      makeButton("6", "", function () { addNumber("6"); }),
-      makeButton("-", "operator", function () { chooseOperator("-"); }),
-
-      makeButton("1", "", function () { addNumber("1"); }),
-      makeButton("2", "", function () { addNumber("2"); }),
-      makeButton("3", "", function () { addNumber("3"); }),
-      makeButton("+", "operator", function () { chooseOperator("+"); }),
-
-      makeButton("DEL", "gray", deleteLast),
-      makeButton("0", "", function () { addNumber("0"); }),
-      makeButton(".", "", addDot),
-      makeButton("=", "operator", calculate)
-    ),
-    e("p", { className: "note" }, "By Mohamed Anwar")
-  );
 }
 
-var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(e(App));
+function addDot() {
+  var screen = document.getElementById("screen");
+
+  if (screen.value.indexOf(".") == -1) {
+    screen.value = screen.value + ".";
+  }
+}
+
+function clearScreen() {
+  document.getElementById("screen").value = "0";
+  firstNumber = 0;
+  mathOperator = "";
+  startNewNumber = false;
+}
+
+function setOperator(op) {
+  firstNumber = Number(document.getElementById("screen").value);
+  mathOperator = op;
+  startNewNumber = true;
+}
+
+function calculate() {
+  var screen = document.getElementById("screen");
+  var secondNumber = Number(screen.value);
+  var answer = 0;
+
+  if (mathOperator == "+") {
+    answer = firstNumber + secondNumber;
+  }
+
+  if (mathOperator == "-") {
+    answer = firstNumber - secondNumber;
+  }
+
+  if (mathOperator == "*") {
+    answer = firstNumber * secondNumber;
+  }
+
+  if (mathOperator == "/") {
+    if (secondNumber == 0) {
+      screen.value = "Error";
+      return;
+    }
+    answer = firstNumber / secondNumber;
+  }
+
+  screen.value = answer;
+  startNewNumber = true;
+}
+
+function changeSign() {
+  var screen = document.getElementById("screen");
+  screen.value = Number(screen.value) * -1;
+}
+
+function percentage() {
+  var screen = document.getElementById("screen");
+  screen.value = Number(screen.value) / 100;
+}
+
+function deleteNumber() {
+  var screen = document.getElementById("screen");
+
+  if (screen.value.length > 1) {
+    screen.value = screen.value.substring(0, screen.value.length - 1);
+  } else {
+    screen.value = "0";
+  }
+}
